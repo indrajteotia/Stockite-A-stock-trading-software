47ac239c1d03919a10578daacd46250621867dc4
Indraj Singh Teotia <instamauj@gmail.com>
2022-12-21 00:32:51 +0530

    Upload Files
    all working files are here

---
+++ src/stock/Stock.java
@@ -0,0 +1,13 @@
+
+package stock;
+
+/**
+ *
+ * @author indrajteotia
+ */
+
+public class Stock {
+    public static void main(String[] args){
+                new A_Welcome().setVisible(true);
+    }
+}
